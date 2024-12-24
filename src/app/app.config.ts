43231c04f9a61958a0eb080d1d14e5719322b273
OrbitalCardinal <raulcepedac1@gmail.com>
2024-12-24 00:19:36 -0600

    Upgrade to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { ThemeService } from '@services/theme.service';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { spinnerInterceptor } from '@interceptors/spinner.interceptor';
 import { SpinnerService } from '@services/spinner.service';
 
